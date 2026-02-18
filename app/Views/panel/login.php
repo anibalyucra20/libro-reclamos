b20521bac596c790c71fe531170c3ac4c32c7462
anibal yucra <105953269+anibalyucra20@users.noreply.github.com>
2026-02-18 09:56:34 -0500

    Update login.php

--- app/Views/panel/login.php
+++ app/Views/panel/login.php
@@ -12,11 +12,11 @@ if ($__panelPrefix === '') $__panelPrefix = '/panel';
   <input type="hidden" name="_csrf" value="<?= htmlspecialchars($csrf) ?>">
   <div class="form-group">
     <label>Email</label><br>
-    <input type="email" name="email" class="form-control" required><br><br>
+    <input type="email" name="email" class="form-control col-md-4" required><br><br>
   </div>
   <div class="form-group">
     <label>Password</label><br>
-    <input type="password" name="password" class="form-control" required><br><br>
+    <input type="password" name="password" class="form-control col-md-4" required><br><br>
   </div>
   <div class="form-group">
     <button type="submit" class="btn btn-success">Ingresar</button>
